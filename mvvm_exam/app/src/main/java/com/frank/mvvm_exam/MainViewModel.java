4febe76e206bf472f40944ba7a2094418b86eea6
choi jung hyun <cjhnim@gmail.com>
2019-11-12 19:43:53 +0900

    mvvm_exam: add number validate feature

--- mvvm_exam/app/src/main/java/com/frank/mvvm_exam/MainViewModel.java
+++ mvvm_exam/app/src/main/java/com/frank/mvvm_exam/MainViewModel.java
@@ -1,24 +1,51 @@
 package com.frank.mvvm_exam;
 
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Observable;
 import androidx.databinding.ObservableField;
 
-public class MainViewModel {
-    public final ObservableField<String> result = new ObservableField<>();
-    public final ObservableField<String> number1 = new ObservableField<>();
-    public final ObservableField<String> number2 = new ObservableField<>();
+public class MainViewModel extends BaseObservable implements Observable{
+    public final ObservableField<String> result = new ObservableField<>("");
+    public final ObservableField<String> number1 = new ObservableField<>("");
+    public final ObservableField<String> number2 = new ObservableField<>("");
     private Calculator calculator;
+    private OnPropertyChangedCallback changedCallback;
+
+    public final ObservableField<String> number1Error = new ObservableField<>("");
+    public final ObservableField<String> number2Error = new ObservableField<>("");
+    String pattern = "[0-9]+";
 
     public MainViewModel(Calculator calculator) {
         this.calculator = calculator;
-        result.set("");
-        number1.set("");
-        number2.set("");
+        changedCallback = new OnPropertyChangedCallback() {
+            @Override
+            public void onPropertyChanged(Observable sender, int propertyId) {
+                if(sender == number1)
+                    validateNumber(number1, number1Error);
+                else if(sender == number2)
+                    validateNumber(number2, number2Error);
+            }
+        };
+
+        number1.addOnPropertyChangedCallback(changedCallback);
+        number2.addOnPropertyChangedCallback(changedCallback);
+    }
+
+    private void validateNumber(ObservableField<String> number, ObservableField<String> numberError) {
+        if(notValid(number))
+            numberError.set("invalid input");
+        else
+            numberError.set("");
+    }
+
+    private boolean notValid(ObservableField<String> number) {
+        return !number.get().isEmpty() && !number.get().matches(pattern);
     }
 
     public void resultClick() {
-        int n1 = Integer.parseInt(number1.get());
-        int n2 = Integer.parseInt(number2.get());
-        result.set(Integer.toString( calculator.sum(n1,n2)));
+        int n1 = notValid(number1) ?  0 : Integer.parseInt(number1.get());
+        int n2 = notValid(number2) ? 0 : Integer.parseInt(number2.get()) ;
 
+        result.set(Integer.toString( calculator.sum(n1,n2)));
     }
 }
--- mvvm_exam/app/src/test/java/com/frank/mvvm_exam/MainViewModelTest.java
+++ mvvm_exam/app/src/test/java/com/frank/mvvm_exam/MainViewModelTest.java
@@ -26,4 +26,48 @@ public class MainViewModelTest {
         viewModel.resultClick();
         assertEquals("3", viewModel.result.get());
     }
+
+    @Test
+    public void InvalidNumber() {
+
+        viewModel.number1.set("abcd");
+        assertEquals("invalid input", viewModel.number1Error.get());
+    }
+
+    @Test
+    public void ValidNumber() {
+
+        viewModel.number1.set("123");
+        assertEquals("", viewModel.number1Error.get());
+    }
+
+    @Test
+    public void ValidNumberAfterInvalid() {
+        viewModel.number1.set("abc");
+        viewModel.number1.set("123");
+        assertEquals("", viewModel.number1Error.get());
+    }
+
+    @Test
+    public void Blank_ShouldValid() {
+        viewModel.number1.set("abc");
+        viewModel.number1.set("");
+        assertEquals("", viewModel.number1Error.get());
+    }
+
+    @Test
+    public void InvalidNumberOfNumber2() {
+
+        viewModel.number2.set("abcd");
+        assertEquals("invalid input", viewModel.number2Error.get());
+    }
+
+    @Test
+    public void WhenInvalidInput_Then_ShouldNotCalculate() {
+        viewModel.number1.set("abcd");
+        viewModel.number2.set("abcd");
+        viewModel.resultClick();
+
+        assertEquals("0", viewModel.result.get());
+    }
 }
